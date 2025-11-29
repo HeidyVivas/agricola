ee9e7d178071e5b7a638f8ed19d968a363e7492b
Heidy-Vivas <hvivas2208@gmail.com>
2025-11-28 21:56:47 -0500

    Añade la aplicación de cuentas con vistas de registro y autenticación JWT

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cosechas',
     'perdidas',
     'reportes',
+	'accounts',
 ]
 
 # Middlewares
@@ -122,6 +123,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+		
         # si deseas seguir soportando SessionAuth:
         # 'rest_framework.authentication.SessionAuthentication',
     ),
